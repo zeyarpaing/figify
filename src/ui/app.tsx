aaa1076bd017231ca30463ca914679de09613615
ZeyarPaing <zeyarpaing@protonmail.com>
2023-12-05 23:10:08 +0630

    Update sample UI

--- src/ui/app.tsx
+++ src/ui/app.tsx
@@ -8,39 +8,57 @@ export function App() {
   return (
     <>
       <main class="w-full h-full p-6">
-        <h1 class="dark:text-teal-500 text-blue-500 font-bold text-xl mb-2">Rectangle Creator</h1>
-        <label class="text-sm">
-          Ractangle count:{' '}
-          <input
-            class="rounded text-md w-full px-3 py-2 text-black"
-            type="number"
-            id="count"
-            value={count}
-            //  @ts-ignore
-            onChange={(e) => setCount(+e.target?.value)}
-          />
-        </label>
-        <div class="flex gap-2 items-center  justify-center my-2">
-          <Button
-            id="fill"
-            onClick={() => {
-              Figma.fillText(['Hello world!', 'Hi mom', "I'm a rectangle", 'Hi dad']);
-            }}
-          >
-            Fill Random Text
-          </Button>
-          <Button
-            id="create"
-            onClick={() => {
-              Figma.createRectangles(count);
-            }}
-          >
-            Create
-          </Button>
+        <h1 class="font-bold text-xl mb-2">Figify</h1>
+        <small class="text-opacity-70">
+          Spin up a figma plugin development project in seconds, with your favourite stack.
+        </small>
+        <div class="mt-4 flex flex-col space-y-4">
+          <label class="text-sm">
+            <p>Create squares</p>
+            <div class="flex mt-1 items-center">
+              <input
+                class="rounded-l-md text-md w-full px-3 py-2 dark:bg-slate-600"
+                type="number"
+                id="count"
+                value={count}
+                //  @ts-ignore
+                onInput={(e) => setCount(+e.target?.value)}
+              />
+              <Button
+                class="py-2 rounded-r-md rounded-l-none "
+                onClick={() => {
+                  Figma.createRectangles(count);
+                }}
+              >
+                Create
+              </Button>
+            </div>
+          </label>
+          <label class="text-sm">
+            <p>Fill random text</p>
+            <div class="flex mt-1 items-center">
+              <input
+                disabled
+                class="rounded-l-md text-md w-full px-3 py-2 disabled:bg-slate-300 dark:disabled:bg-slate-700"
+                value={`["Hello world!", "Hi mom", "I'm a rectangle", "Hi dad"]`}
+              />
+              <Button
+                class="py-2 rounded-r-md rounded-l-none"
+                onClick={() => {
+                  Figma.fillText(['Hello world!', 'Hi mom', "I'm a rectangle", 'Hi dad']);
+                }}
+              >
+                Fill
+              </Button>
+            </div>
+          </label>
         </div>
 
-        <Button id="create" onClick={() => Figma.cancel()}>
-          Close
+        <Button
+          class="bg-red-500/30 text-red-400 hover:bg-red-500/40 text-xs font-medium absolute bottom-6 right-6"
+          onClick={() => Figma.cancel()}
+        >
+          Close plugin
         </Button>
       </main>
     </>
--- src/ui/components/Button.tsx
+++ src/ui/components/Button.tsx
@@ -1,9 +1,17 @@
 import { JSX } from 'preact';
+import { twMerge } from 'tailwind-merge';
 
-export function Button(props: JSX.HTMLAttributes<HTMLButtonElement>) {
+export function Button({
+  class: $class,
+  className,
+  ...props
+}: JSX.HTMLAttributes<HTMLButtonElement>) {
   return (
     <button
-      class="bg-blue-500 hover:bg-blue-700 text-white text-sm font-bold py-1 px-3 rounded"
+      class={twMerge(
+        'bg-blue-500 hover:bg-blue-700 text-white text-sm font-bold py-1 px-3 rounded',
+        ($class || className) as string
+      )}
       {...props}
     />
   );
